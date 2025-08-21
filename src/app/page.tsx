4decc62342e416ed8fdaa7c4d76ab7be511f18d6
alfaazrilomega <alfajriazril1@gmail.com>
2025-08-21 20:53:39 +0700

    feat: make Table component generic and implement it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,34 @@
-import Table from "@/components/Table";
-import { employees } from "@/data/employee";
+import Table, { ColumnDefs } from "@/components/Table";
+import { employees, Employee } from "@/data/employee";
 
 export default function Home() {
+  const columnDefs: ColumnDefs<Employee>[] = [
+    {
+      title: "Name",
+      render: (rowData) => `${rowData.firstName} ${rowData.lastName}`
+    },
+    {
+      title: "Age",
+      field: "age"
+    },
+    {
+      title: "Email",
+      field: "email"
+    },
+    {
+      title: "Phone",
+      field: "phone"
+    }
+  ]
   return (
     <main className="flex min-h-screen items-center justify-around bg-slate-50">
-
       <section>
-        <h1 className="text-4xl font-bold mb-8 text-gray-800 text-center">Non Generic Table</h1>
-        <Table columnDefs={[
-          {
-            title: "Name",
-            render: (rowData) => `${rowData.firstName} ${rowData.lastName}`
-          },
-          {
-            title: "Age",
-            field: "age"
-          },
-          {
-            title: "Email",
-            field: "email"
-          },
-          {
-            title: "Phone",
-            field: "phone"
-          }
-        ]}
+        <h1 className="text-4xl font-bold mb-8 text-gray-800 text-center">Generic  BEGE</h1>
+        <Table<Employee>
+          columnDefs={columnDefs}
           data={employees}
         />
       </section>
-
     </main>
   );
 }
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,40 +1,45 @@
 import React from "react";
 
-export type ColumnDefs = {
-    title: string;
-} & (
-        | {
-            field: string;
-        }
-        | {
-            render: (rowData: Record<string, any>) => React.ReactNode;
-        }
-    );
+export type ColumnDefs<T> =
+    | {
+        title: string;
+        field: keyof T;
+    }
+    | {
+        title: string;
+        render: (rowData: T) => React.ReactNode;
+    };
 
-const Table = ({
+type TableProps<T> = {
+    columnDefs: ColumnDefs<T>[];
+    data: T[];
+};
+
+function Table<T extends Record<string, any>>({
     columnDefs,
     data,
-}: {
-    columnDefs: ColumnDefs[];
-    data: Record<string, any>[];
-}) => {
+}: TableProps<T>) {
     return (
         <table className="min-w-full divide-y overflow-hidden rounded-lg divide-gray-800 shadow-lg">
             <thead className="bg-green-200">
                 <tr>
-                    {columnDefs.map((column, index) => (
-                        <th key={index} scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    {columnDefs.map((column, idx) => (
+                        <th key={idx} scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                             {column.title}
                         </th>
                     ))}
                 </tr>
             </thead>
             <tbody className="bg-white divide-y divide-gray-200">
-                {data.map((rowData, index) => (
-                    <tr key={index} className="cursor-pointer hover:bg-gray-100">
-                        {columnDefs.map((column, index) => (
-                            <td key={index} className="px-6 py-4 whitespace-nowrap">
-                                {"field" in column ? rowData[column.field!] : column.render ? column.render(rowData) : null}
+                {data.map((rowData, rowIdx) => (
+                    <tr key={rowIdx} className="cursor-pointer hover:bg-gray-100">
+                        {columnDefs.map((column, colIdx) => (
+                            <td key={colIdx} className="px-6 py-4 whitespace-nowrap">
+                                {"field" in column
+                                    ? rowData[column.field]
+                                    : "render" in column
+                                    ? column.render(rowData)
+                                    : null}
                             </td>
                         ))}
                     </tr>
@@ -42,6 +47,6 @@ const Table = ({
             </tbody>
         </table>
     );
-};
+}
 
 export default Table;
